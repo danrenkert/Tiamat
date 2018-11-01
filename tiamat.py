aad9c90394b4af77d175584fe3e7542fad8e911a
Jmatt512 <35381701+Jmatt512@users.noreply.github.com>
2018-11-01 01:21:46 -0600

    Update tiamat.py

--- tiamat.py
+++ tiamat.py
@@ -1,31 +1,51 @@
 """
  Tiamat controll 
+ 
+ to do:
+    add tstign to make sure user inputs a valid input
+    make sure a cue exists, dont let it fail if the cue does not exists
+    test the software on a compter with the serial conected to the master arduino
+    
 """
 import serial
 cues=[]
 temp=str()
 #ser = serial.Serial('USB\VID_1A86&PID_7523\5&34C8F5E4&0&7', 9600, timeout=30)
-ser = serial.Serial('USB\VID_1A86&PID_7523\5&2C705BFE&0&1', 9600, timeout=30)
+#ser = serial.Serial('USB\VID_1A86&PID_7523\5&2C705BFE&0&1', 9600, timeout=30)
 print("test")
 with open('test.txt', 'r') as f:
     for line in f:
         temp = line
-        print (temp)
         cues.append(temp)
         
    
      
 for item in cues:
     print(item)
-    print("\n")
 print("endtest")
 
-curentCue = 1
-
-
-newCue=input("Enter cue number")
-print(cues[newCue])
-ser.write(cues[newCue])
+curentCue = -1
+newCue=str()
+while True:
+    #get user input 
+    newCue=raw_input("Enter cue number   ")
+    #if they enter enter go to the next cue
+    if newCue=="":
+       newCue=curentCue+1
+    #if they enter b go back to the last cue
+    elif newCue=="b" or newCue=="B":
+        newCue=curentCue-1
+    #otherwise pray to god tey entered a number and go to that cue
+    else:
+        newCue=int(newCue)-1
+        
+    #print the current cue for testing 
+    print(cues[int(newCue)])
+    #set the current cue to the actual current cue
+    curentCue=newCue
+    
+    #write to serial out the cue, this will have to be uncommented once its acutaly used
+    #ser.write(cues[int(newCue)])
 
 
 
@@ -34,7 +54,8 @@ ser.write(cues[newCue])
 
 """"
 
-these notes are just part of a old project i did that was similar to this. hopefuly it helps
+everythign below here is from another project i did similar to this it might help
+
 
 # read comands from comand file
 ser = serial.Serial()#'/dev/ttyACM0', 9600, timeout=30)
